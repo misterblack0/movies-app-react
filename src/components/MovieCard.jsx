b0578159d4aa92adcd50bc98542ba83d8fbfb944
misterblack0 <misterblack0@protonmail.com>
2020-09-17 17:56:02 +0300

    m

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -30,51 +30,23 @@ function MovieCard({ movie }) {
           <div className="bubble bubble-fill">
             <div className="bubble__container1">
               <div className="bubble__container1--header">
-                <div>
-                  <h3 className="card__title">{movie.title}</h3>
-                  <small className="card__genres">{movie.genre_ids}</small>
-                </div>
-                <div>
-                  <p>1h 57 min</p>
-                </div>
+                <h3 className="card__title">{movie.title}</h3>
+
+                <p>1h 57 min</p>
               </div>
 
               <p className="card__overview">{movie.overview}</p>
-
-              <div className="card__cast">
-                <p>Director:</p>
-                <p>Stars:</p>
-              </div>
             </div>
 
             <div className="bubble__container2">
-              <div>
-                <p>{movie.original_language}</p>
-                <p>
-                  <a href="https://google.ro" className="card__link">
-                    IMDb
-                  </a>
-                </p>
-              </div>
-              <div>
-                <p>{movie.vote_average}</p>
-                <p>
-                  <a href="https://google.ro" className="card__link">
-                    Metacritic
-                  </a>
-                </p>
-              </div>
-            </div>
-
-            <div className="bubble__container3">
               <p>Recommended by:</p>
 
-            <div>
-            <img src="/img/profile_user7.jpg" alt="user"></img>
-            <img src="/img/profile_user6.jpg" alt="user"></img>
-            <img src="/img/profile_user5.jpg" alt="user"></img>
-            <img src="/img/profile_user4.jpg" alt="user"></img>
-</div>
+              <div>
+                <img src="/img/profile_user7.jpg" alt="user"></img>
+                <img src="/img/profile_user6.jpg" alt="user"></img>
+                <img src="/img/profile_user5.jpg" alt="user"></img>
+                <img src="/img/profile_user4.jpg" alt="user"></img>
+              </div>
             </div>
           </div>
         </div>
@@ -82,14 +54,6 @@ function MovieCard({ movie }) {
 
       <div className="card__content">
         <h3 className="card__title">{movie.title}</h3>
-        <p>
-          <small className="card__genres">Drama, Thriller</small>
-        </p>
-
-        {/*  <p>
-          <small>RATING: {movie.vote_average}</small>
-        </p>
-        <p className="card--description">{movie.overview}</p> */}
       </div>
     </div>
   );
